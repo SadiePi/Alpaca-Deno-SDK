9972aac6973891f12e4f3fd1394b86579db03eb3
Aria Dotzler <sadiedotzler@gmail.com>
2025-06-15 18:31:45 -0700

    Order string literals

--- trading/orders.ts
+++ trading/orders.ts
@@ -1,2 +1,20 @@
+export type OrderClass = "simple" | "bracket" | "oco" | "oto" | "mleg";
+export type OrderSide = "buy" | "sell";
+export type OrderType =
+  | "market"
+  | "limit"
+  | "stop"
+  | "stop_limit"
+  | "trailing_stop";
+export type TimeInForce = "day" | "gtc" | "opg" | "cls" | "ioc" | "fok";
+export type OrderLeg = unknown;
+export type OrderTakeProfit = unknown;
+export type OrderStopLoss = unknown;
+export type PositionIntent =
+  | "buy_to_open"
+  | "buy_to_close"
+  | "sell_to_open"
+  | "sell_to_close";
+
 export type OrderBody = unknown;
 export type OrdersQuery = unknown;
